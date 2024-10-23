a70c7357efb864181995274b68add9a2d5b86b3a
Nicolas-2701 <ngalvao388@gmail.com>
2024-10-23 08:23:59 -0300

    inicio

---
+++ src/main/java/com/ngo/tomada_inteligente/TomadaInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.ngo.tomada_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomadaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomadaInteligenteApplication.class, args);
+	}
+
+}
