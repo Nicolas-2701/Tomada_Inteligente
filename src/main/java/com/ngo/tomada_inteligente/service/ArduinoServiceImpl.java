246b12e3f2d08b4a4caf80243053a2764ae4a12e
tandrou <miguelmateuscelbitmmc@gmail.com>
2024-10-24 11:31:57 -0300

    fix comunicação de serial

--- src/main/java/com/ngo/tomada_inteligente/service/ArduinoServiceImpl.java
+++ src/main/java/com/ngo/tomada_inteligente/service/ArduinoServiceImpl.java
@@ -7,24 +7,24 @@ import com.ngo.tomada_inteligente.model.DadosArduino;
 
 @Service
 public class ArduinoServiceImpl implements ArduinoService {
-	
-	@Override
-	public void comunicacaoS(int porta) {
-		//transforma a porta em serial port e abre ela
-		SerialPort portS = SerialPort.getCommPorts()[porta];
-		portS.openPort();
-		
-		if (portS.bytesAvailable() > 0) {
+
+    @Override
+    public void comunicacaoS(int porta) {
+        //transforma a porta em serial port e abre ela
+        SerialPort portS = SerialPort.getCommPorts()[porta];
+        portS.openPort();
+        if (portS.bytesAvailable() > 0) {
             byte[] buffer = new byte[portS.bytesAvailable()];
             portS.readBytes(buffer, buffer.length);
 
-            String valor = new String(buffer).trim();
+            String valor = new String(buffer);
             System.out.println("Valor recebido do Arduino: " + valor);
 
             enviar(valor); // Enviar o valor para a API
-        }
-	}
-	private static void enviar(String valor) {
+            portS.closePort();
+            }
+    }
+    private static void enviar(String valor) {
         DadosArduino dados = new DadosArduino();
         dados.setValor(valor);
     }
