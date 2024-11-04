2be6297a035b35d1e594ce0b7f4033aa7ad6f78b
Nicolas-2701 <ngalvao388@gmail.com>
2024-11-04 17:10:20 -0300

    add - bd h2 + model>home

--- src/main/java/com/ngo/tomada_inteligente/controller/TomadaController.java
+++ src/main/java/com/ngo/tomada_inteligente/controller/TomadaController.java
@@ -1,19 +1,29 @@
 package com.ngo.tomada_inteligente.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ngo.tomada_inteligente.model.Tomada;
+import com.ngo.tomada_inteligente.repository.TomadaRepository;
 import com.ngo.tomada_inteligente.service.ArduinoService;
 
 @Controller
 public class TomadaController {
 	
 	@Autowired
+	private TomadaRepository tRepo;
+	
+	@Autowired
 	private ArduinoService arduino;
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		List<Tomada> tomadas =  tRepo.findAll();
+		model.addAttribute("tomadas", tomadas);
 		String [] dados = arduino.separarDados(arduino.comunicacaoS(0));//[0] = watts; [1] = volts que calculam a corrente(remover); [2] = corrente
 		return "home";
 	}
